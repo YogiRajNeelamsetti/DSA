75d772bc30f6b26fed7c310800fc92ee2be97c07
NEELAMSETTI YOGIRAJ <40514567+YogiRajNeelamsetti@users.noreply.github.com>
2025-01-14 17:00:25 +0530

    Time: 0 ms (100.00%) | Memory: 128.2 MB (74.64%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        int posIdx = 0, negIdx = 1;
         vector<int> res(nums.size());
+        int posIdx = 0, negIdx = 1;
 
-        for(auto num : nums) {
+        for(int num : nums) {
             if(num > 0) {
                 res[posIdx] = num;
                 posIdx += 2;
