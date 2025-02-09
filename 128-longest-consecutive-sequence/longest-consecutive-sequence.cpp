84154bc6230d50c2a225a539a9a31cd7c90997bf
Yogi Raj <40514567+YogiRajNeelamsetti@users.noreply.github.com>
2025-02-09 16:55:03 +0530

    Time: 18 ms (78.57%) | Memory: 59 MB (81.65%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -4,22 +4,15 @@ public:
         int n = nums.size();
         if(n == 0) return 0;
         int longest = 1;
+        int count = 1;
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < n; i++) {
+            if(nums[i] - nums[i - 1] == 1) count++;
+            else if(nums[i] - nums[i - 1] == 0) continue;
+            else count = 1;
 
-        unordered_set<int> s;
-
-        for(int i = 0; i < n; i++) {
-            s.insert(nums[i]);
-        }
-
-        for(auto it : s) {
-            if(s.find(it - 1) == s.end()) {
-                int cnt = 1;
-                int x = it;
-                while(s.find(x + 1) != s.end()) {
-                    x++;
-                    cnt++;
-                }
-                longest = max(longest, cnt);
+            if(count > longest) {
+                longest = count;
             }
         }
 
