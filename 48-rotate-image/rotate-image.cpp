11ce0987211db43b568054efb3cc43677f8b0728
Yogi Raj <40514567+YogiRajNeelamsetti@users.noreply.github.com>
2025-02-09 19:10:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (8.68%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -4,14 +4,10 @@ public:
         int n = matrix.size();
         vector<vector<int>> arr(n, vector<int>(n));
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                arr[j][n - 1 - i] = matrix[i][j];
+            for(int j = i + 1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
             }
-        }
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                matrix[i][j] = arr[i][j];
-            }
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
