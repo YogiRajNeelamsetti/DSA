38f7fe4ccaf21b8fbff91c9fecbd1072e6fb9fe9
Yogi Raj <40514567+YogiRajNeelamsetti@users.noreply.github.com>
2025-03-09 15:37:44 +0530

    Time: 7 ms (88.44%) | Memory: 14.1 MB (23.78%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -7,7 +7,8 @@ public:
         for(int i = 0; i < n - 2; i++) {
             int p1 = i + 1, p2 = n - 1;
             while(p1 < p2) {
-                int sum = nums[p1] + nums[p2] + nums[i];
+                int sum = nums[p1] + nums[p2];
+                sum += nums[i];
                 if(abs(sum - target) < abs(closest - target)) {
                     closest = sum;
                 }
