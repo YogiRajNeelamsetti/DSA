1fb4fec5eea08f424e8553d637d480f92087e872
Yogi Raj <40514567+YogiRajNeelamsetti@users.noreply.github.com>
2025-06-14 17:55:40 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (33.65%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+
+        int maxLen = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(i > maxLen) return false;
+
+            maxLen = max(maxLen, i + nums[i]);
+        }
+
+        return true;
+    }
+};
