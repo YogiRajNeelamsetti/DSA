71af718043f09691a67ff5a0580ed99d08abf91f
Yogi Raj <40514567+YogiRajNeelamsetti@users.noreply.github.com>
2025-06-25 06:26:57 +0530

    Time: 4 ms (8.19%) | Memory: 13.3 MB (11.64%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> prev(n);
+
+        for(int j = 0; j < n; j++) {
+            prev[j] = triangle[n - 1][j];
+        }
+
+        for(int i = n - 2; i >= 0; i--) {
+            vector<int> curr(i + 1);
+            for(int j = i; j >= 0; j--) {
+                int down = triangle[i][j] + prev[j];
+                int downRight = triangle[i][j] + prev[j + 1];
+
+                curr[j] = min(down, downRight);
+            }
+            prev = curr;
+        }
+
+        return prev[0];
+    }
+};
