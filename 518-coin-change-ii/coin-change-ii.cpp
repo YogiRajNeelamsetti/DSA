9c6415bfdd582931ebd3e0e2c4fe695239338555
Yogi Raj <40514567+YogiRajNeelamsetti@users.noreply.github.com>
2025-07-05 11:28:08 +0530

    Time: 2 ms (96.99%) | Memory: 10.1 MB (94.92%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -4,23 +4,16 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
 
-        vector<vector<uint64_t>> dp(n, vector<uint64_t>(amount + 1, 0));
+        vector<unsigned long long> dp(amount + 1, 0);
 
-        for(int t = 0; t <= amount; t++) {
-            if(t % coins[0] == 0) dp[0][t] = 1;
-        }
+        dp[0] = 1;
 
-        for(int i = 1; i < n; i++) {
-            for(int t = 0; t <= amount; t++) {
-                uint64_t notTake = dp[i - 1][t];
-                uint64_t take = 0;
-
-                if(coins[i] <= t) take = dp[i][t - coins[i]];
-
-                dp[i][t] = take + notTake;
+        for(long long coin : coins) {
+            for(long long t = coin; t <= amount; t++) {
+                dp[t] = dp[t] + dp[t - coin];
             }
         }
 
-        return dp[n - 1][amount];
+        return dp[amount];
     }
 };
