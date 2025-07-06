32768670b25a9c009f418c1648fc6f2fcc8eb4fb
Yogi Raj <40514567+YogiRajNeelamsetti@users.noreply.github.com>
2025-07-06 13:23:37 +0530

    Time: 27 ms (55.45%) | Memory: 13.3 MB (81.25%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -5,15 +5,20 @@ public:
         int m = text2.size();
 
         vector<int> prev(m + 1, 0), curr(m + 1, 0);
-
+        string ans = "";
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= m; j++) {
-                if(text1[i - 1] == text2[j - 1]) curr[j] = 1 + prev[j - 1];
+                if(text1[i - 1] == text2[j - 1]) {
+                    curr[j] = 1 + prev[j - 1];
+                    ans += text1[i - 1];
+                }
                 else curr[j] = max(curr[j - 1], prev[j]);
             }
             prev = curr;
 
         }
+
+        cout<<ans<<endl;
         return prev[m];
     }
 };
