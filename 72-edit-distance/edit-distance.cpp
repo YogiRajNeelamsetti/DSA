b2526208ed8f9950c586d90a90200657f1a7de7f
Yogi Raj <40514567+YogiRajNeelamsetti@users.noreply.github.com>
2025-07-06 18:53:35 +0530

    Time: 11 ms (29.60%) | Memory: 10.5 MB (90.74%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -16,10 +16,26 @@ public:
     }
     int minDistance(string word1, string word2) {
         int n = word1.size(), m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
 
-        return f(word1, word2, dp, n - 1, m - 1);
+        for(int j = 0; j <= m; j++) prev[j] = j;
 
-        // return dp[n - 1][m - 1];
+        for(int i = 1; i <= n; i++) {
+            curr[0] = i;
+            for(int j = 1; j <= m; j++) {
+                if(word1[i - 1] == word2[j - 1]) {
+                    curr[j] = prev[j - 1];
+                } else {
+                    curr[j] = min({
+                        1 + prev[j],
+                        1 + curr[j - 1],
+                        1 + prev[j - 1]
+                    });
+                }
+            }
+            prev = curr;
+        }
+
+        return (int)prev[m];
     }
 };
