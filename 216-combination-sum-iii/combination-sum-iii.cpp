e615c3a31a32cfa1dc673aa6af1073c52b38c814
Yogi Raj <40514567+YogiRajNeelamsetti@users.noreply.github.com>
2025-07-16 14:25:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (95.21%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    void combination(int ind, vector<vector<int>> &ans, vector<int> &ds, int target, int k) {
-        if(ds.size() == k) {
-            if(target == 0) {
-                ans.push_back(ds);
-            }
+    void combination(int ind, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds, int target, int k) {
+        if(target == 0 && ds.size() == k) {
+            ans.push_back(ds);
             return;
         }
         if(ds.size() > k) return;
 
-        for(int i = ind; i <= 9; i++) {
-            if(i > target) break;
-            ds.push_back(i);
-            combination(i + 1, ans, ds, target - i, k);
+        for(int i = ind; i < arr.size(); i++) {
+            if(arr[i] > target) break;
+            ds.push_back(arr[i]);
+            combination(i + 1, arr, ans, ds, target - arr[i], k);
             ds.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-
+        vector<int> arr(9);
+        for(int i = 0; i < 9; i++) {
+            arr[i] = i + 1;
+        }
         vector<vector<int>> ans;
         vector<int> ds;
 
-        combination(1, ans, ds, n, k);
+        combination(0, arr, ans, ds, n, k);
 
         return ans;
     }
