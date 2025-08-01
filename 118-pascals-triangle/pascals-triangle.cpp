6a5550f94db3d08b54942241e07aa9f3c401a66d
Yogi Raj <40514567+YogiRajNeelamsetti@users.noreply.github.com>
2025-08-01 11:33:18 +0530

    Time: 11 ms (0.68%) | Memory: 9.6 MB (62.59%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
 
-    vector<int> nRow(int r) {
-        long long ans = 1;
-        vector<int> row;
-        row.push_back(ans);
-        for(int i = 1; i < r; i++) {
-            ans = ans * (r - i);
-            ans = ans / i;
-            row.push_back(ans);
-        }
-
-        return row;
-    }
-
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> pascalTriangle;
 
-        for(int i = 1; i <= numRows; i++) {
-            pascalTriangle.push_back(nRow(i));
+        for(int r = 1; r <= numRows; r++) {
+            long long ans = 1;
+            vector<int> row;
+            row.push_back(ans);
+            for(int i = 1; i < r; i++) {
+                ans = ans * (r - i);
+                ans = ans / i;
+                row.push_back(ans);
+            }
+
+            for(int ele : row) cout<<ele<<" ";
+            cout<<endl;
+            pascalTriangle.push_back(row);
         }
 
         return pascalTriangle;
