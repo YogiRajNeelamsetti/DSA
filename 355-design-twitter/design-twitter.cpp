f521ec157362ece1e4aae872db5bd35e6d9281ec
Yogi Raj <40514567+YogiRajNeelamsetti@users.noreply.github.com>
2026-05-08 19:58:58 +0530

    Time: 5 ms (72.71%) | Memory: 56.7 MB (33.71%) - LeetSync

---
+++ 355-design-twitter/design-twitter.cpp
@@ -0,0 +1,60 @@
+class Twitter {
+public:
+    int count;
+    unordered_map<int, unordered_set<int>> followMap;
+    unordered_map<int, vector<pair<int, int>>> tweetMap;
+    Twitter() {
+        count = 0;
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        tweetMap[userId].push_back({count, tweetId});
+        if(tweetMap[userId].size() > 10) {
+            tweetMap[userId].erase(tweetMap[userId].begin());
+        }
+        count++;
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        vector<int> res;
+        priority_queue<vector<int>> pq;
+        followMap[userId].insert(userId);
+        for(auto f : followMap[userId]) {
+            if(!tweetMap.count(f)) continue;
+
+            int idx = tweetMap[f].size() - 1;
+            auto &p = tweetMap[f][idx];
+            pq.push({p.first, p.second, f, idx - 1});
+        }
+        while(!pq.empty() && res.size() < 10) {
+            auto t = pq.top();
+            pq.pop();
+            int idx = t[3];
+            res.push_back(t[1]);
+            if(idx >= 0) {
+                auto &p = tweetMap[t[2]][idx];
+                pq.push({p.first, p.second, t[2], idx - 1});
+            }
+        }
+        return res;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        followMap[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        if(followMap[followerId].count(followeeId)) {
+            followMap[followerId].erase(followeeId);
+        }
+    }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
